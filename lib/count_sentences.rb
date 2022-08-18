f14dd32046c514dd2101bc94681872b5bcea35dc
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-08-18 16:34:57 +0300

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter {|sentence| !sentence.empty?}.size
   end
 end
+ "Hi,my name is sophie.".sentence?
+  "mambo?".question?
+  "Alaah!".exclamation?
+  "returns the number of sentences".count_sentences
+"This is a string! It has three sentences. Right?".count_sentences
